5342da61bff8670eb4c7cc53eec16ee044778190
LucaAnto01 <luca.antognarelli@gmail.com>
2021-10-21 10:18:33 +0200

    Upgrade makeaccountpro

--- makeAccountPro.sh
+++ makeAccountPro.sh
@@ -3,8 +3,10 @@
 #
 
 #variables
-#FILEUTENTE="/home/esercizi/esercitazione3"
+HELPMESSAGE="Usage: $0 [-h|-v] [file or path]"
+
 #user's choose (-v: verbose mode; -h: Usage: mkaccounts [-h|-v] and exit with status 0; other: Usage: mkaccounts [-h|-v] and exit with status 1)
+
 OPTION=$1
 
 #valutate the value of option
@@ -12,41 +14,49 @@ case $OPTION in
     '') exit 0;;
     -v) verbose=y;;
     -h)
-        echo "Usage: $0 [-h|-v]"
+        echo $HELPMESSAGE
         exit 0;;
     *)
-        echo "Usage: $0 [-h|-v]"
+        echo $HELPMESSAGE
         exit 1;;
 esac
 
+if ! [ $# -ge 2 ]; then
+    echo $HELPMESSAGE
+    exit 2;
+else
+    USERFILE=$2
+fi
+
 #loop for read and split data
-for ENTRY in $(cat $FILEUTENTE); do
+for ENTRY in $(cat $USERFILE); do
+
     #split data when character is :
     USERFIRSTNAME=$(echo $ENTRY | cut -d: -f1) 
     USERLASTNAME=$(echo $ENTRY | cut -d: -f2)
     tail=$(echo $ENTRY | cut -d: -f4)
 
-    #transofrm first character to upper case
-    #cut -c 1 | tr 'a-z' 'A-Z'
-
     #concatenate first and last name for create userName
     USERNAME=$USERFIRSTNAME$USERLASTNAME
 
-    if [ verbose -eq 'y' ]; then
+    if [ "$verbose" = "y" ]; then
         echo "Wait for create user: $USERNAME"
     fi
     
     #create a new account
-    useradd USERNAME -c "$USERFIRSTNAME $USERLASTNAME" 2> /dev/null
+    #it could be necessary to add "sudo"
+    useradd $USERNAME -c "$USERFIRSTNAME $USERLASTNAME" 2> errorsFile
 
-    if [ verbose -eq 'y' ] && [ $? -eq 0 ]; then
+    OUTCOME=$?
+
+    if [ $OUTCOME -eq 0 ] && [ "$verbose" = "y" ]; then
         echo "$USERNAME user has been created"
+    elif [ $OUTCOME -eq 9 ]; then
+        echo "$USERNAME user already exist"
     else
         echo "Impossible to create $USERNAME user"
     fi
 
-    #number of strings ending with :1
-    #grep -c :1$
 done
 
 
